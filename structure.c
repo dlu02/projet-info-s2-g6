881fde3f2c4ba3ff8aca2857a0ee18445c39bf4b
Romain LOIRS <romain.loirs@ensiie.fr>
2020-03-24 20:51:10 +0000

    Upload New File

---
+++ structure.c
@@ -0,0 +1,84 @@
+//
+//  structure.c
+//  
+//
+//  Created by Loirs Romain on 22/03/2020.
+//
+
+#include "carte.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+deck deck_create(){
+  return NULL;
+}
+
+int deck_isEmpty(deck l){
+  return l==NULL;
+}
+
+/* requires : l valide ou NULL / assigns nothing / ensures retourne la liste x::l */
+deck deck_cons(carte x,deck l){
+    deck res;
+    res= (deck) malloc(sizeof(struct node));
+    res->carte=x;
+    res->next=l;
+    return res;
+}
+
+/* requires l valide / assigns *l / ensure oin ajoute x en tête de *l */
+void deck_add(carte x,deck *l){
+    *l = deck_cons(x,*l);
+}
+
+/* requires *l valide / assigns *l / ensures ajoute x dans *l à la fin */
+void deck_add_last(carte x,deck *l){
+    deck q;
+    if (*l==NULL){
+        *l=deck_cons(x,NULL);
+    }
+    else{
+        q=*l;
+        while (q->next != NULL){
+            q=q->next;
+        }
+        q->next=deck_cons(x,NULL);
+    }
+}
+
+void deck_free(deck l){
+    deck tmp;
+    while (l != NULL){
+        tmp=l->next;
+        free(l);
+        l=tmp;
+    }
+}
+
+/* requires *l != NULL / assigns *l / ensues retire le maillon en tête de l */
+carte deck_remove_head(deck *l){
+    carte res= (*l)->carte;
+    deck tmp= (*l)->next;
+    free(*l);
+    *l = tmp;
+    return res;
+}
+
+void deck_print(deck l){
+    if (deck_isEmpty(l)){
+        printf("the deck is empty\n");
+    }
+    else{
+        deck curl=l;
+        while(!deck_isEmpty(curl)){
+            carte_print(curl->carte);
+            curl=curl->next;
+        }
+    }
+}
+
+
+
+
+
