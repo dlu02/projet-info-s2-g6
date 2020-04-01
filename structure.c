dba3b1529405dda021c53a235edd35b9b4da87ca
Romain LOIRS <romain.loirs@ensiie.fr>
2020-04-01 20:07:29 +0000

    Replace structure.c

--- structure.c
+++ structure.c
@@ -13,14 +13,10 @@
 
 
 
-/* construction du deck et destrcution*/
+/* construction du deck et destruction*/
 
 deck deck_create(){
   return NULL;
-}
-
-int deck_isEmpty(deck l){
-  return l==NULL;
 }
 
 deck deck_cons(carte x,deck l){
@@ -41,14 +37,11 @@ void deck_free(deck l){
 }
 
 
-
-
 /*addition d'element*/
 
 void deck_add(carte x,deck *l){
     *l = deck_cons(x,*l);
 }
-
 
 void deck_add_last(carte x,deck *l){
     deck q;
@@ -65,35 +58,28 @@ void deck_add_last(carte x,deck *l){
 }
 
 
+/*fonction de contrôle*/
 
-
-
-/*retirer des elements*/
-
-carte deck_remove_head(deck *l){
-    carte res= (*l)->carte;
-    deck tmp= (*l)->next;
-    free(*l);
-    *l = tmp;
-    return res;
+int deck_isEmpty(deck l){
+  return l==NULL;
 }
 
-carte deck_remove_queue(deck *l){
-    carte res;
-    return res;
+int deck_carteIn(deck l, Nom nom){
+    while (l != NULL){
+        if (carte_equal(l->carte,nom)) return 1;
+        l=l->next;
+    }
+    return 0;
 }
 
-carte deck_remove_carte(deck *l, Nom nom){
-    
+int deck_length(deck l){
+    int i=0;
+    while (l != NULL){
+        i++;
+        l=l->next;
+    }
+    return i;
 }
-
-carte deck_remove_indice(deck *l, int indice){
-    
-}
-
-
-
-/*afficher le deck*/
 
 void deck_print(deck l){
     if (deck_isEmpty(l)){
@@ -110,6 +96,114 @@ void deck_print(deck l){
         }
     }
 }
+
+
+
+/*retirer des elements*/
+
+carte deck_remove_head(deck *l){
+    carte res= (*l)->carte;
+    deck tmp= (*l)->next;
+    free(*l);
+    *l = tmp;
+    return res;
+}
+
+carte deck_remove_queue(deck *l)
+    {
+        carte res;
+     
+        /* Si la liste contient un seul élément */
+        if((*l)->next == NULL)
+        {
+            /* On le libère et on retourne NULL (la liste est maintenant vide) */
+            return deck_remove_head(l);
+        }
+     
+        /* Si la liste contient au moins deux éléments */
+        deck tmp = *l;
+        deck ptmp = *l;
+        /* Tant qu'on n'est pas au dernier élément */
+        while(tmp->next != NULL)
+        {
+            /* ptmp stock l'adresse de tmp */
+            ptmp = tmp;
+            /* On déplace tmp (mais ptmp garde l'ancienne valeur de tmp */
+            tmp = tmp->next;
+        }
+        /* A la sortie de la boucle, tmp pointe sur le dernier élément, et ptmp sur
+        l'avant-dernier. On indique que l'avant-dernier devient la fin de la liste
+        et on supprime le dernier élément */
+        ptmp->next = NULL;
+        res=tmp->carte;
+        free(tmp);
+        return res;
+    }
+
+carte deck_remove_carte(deck *l, Nom nom){
+    carte res;
+    
+    if (carte_equal((*l)->carte,nom)){
+        res=deck_remove_head(l);
+        return res;
+    }
+     
+        /* Si la liste contient au moins deux éléments */
+    deck tmp = *l;
+    deck ptmp = *l;
+    deck nxt= *l;
+        /* Tant qu'on n'est pas au dernier élément */
+    while(tmp->next != NULL && !carte_equal((tmp->carte),nom))
+        {
+            /* ptmp stock l'adresse de tmp */
+            ptmp = tmp;
+            /* On déplace tmp (mais ptmp garde l'ancienne valeur de tmp */
+            tmp = tmp->next;
+            nxt= tmp->next;
+        }
+        /* A la sortie de la boucle, tmp pointe sur le dernier élément, et ptmp sur
+        l'avant-dernier. On indique que l'avant-dernier devient la fin de la liste
+        et on supprime le dernier élément */
+    ptmp->next = nxt;
+    res=tmp->carte;
+    free(tmp);
+    return res;
+}
+
+carte deck_remove_indice(deck *l, int indice){
+    carte res;
+    
+    if (indice==0){
+        res=deck_remove_head(l);
+        return res;
+    }
+     
+        /* Si la liste contient au moins deux éléments */
+    deck tmp = *l;
+    deck ptmp = *l;
+    deck nxt= *l;
+    int i=0;
+        /* Tant qu'on n'est pas au dernier élément */
+    while(tmp->next != NULL && i!=indice)
+        {
+            /* ptmp stock l'adresse de tmp */
+            i++;
+            ptmp = tmp;
+            /* On déplace tmp (mais ptmp garde l'ancienne valeur de tmp */
+            tmp = tmp->next;
+            nxt= tmp->next;
+        }
+        /* A la sortie de la boucle, tmp pointe sur le dernier élément, et ptmp sur
+        l'avant-dernier. On indique que l'avant-dernier devient la fin de la liste
+        et on supprime le dernier élément */
+    ptmp->next = nxt;
+    res=tmp->carte;
+    free(tmp);
+    return res;
+}
+
+
+
 
 
 
